3d961de1351b9926f85a8c49f0670bba7a10677f
Geraeldogo <2030306@upv.edu.mx>
2023-05-12 02:34:37 -0600

    "estoy probando"

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 class Main {
 
     public static void main(String[] args) {
+        int a = integer.parseint(args[0]);
+        int b = integer.parseint(args[1]);
+        
+        SumaDosNumeros obj = new SumaDosNumeros();
+        System.out.printls(obj.suma(a,terb));
+        
+         }
 
     }
-}
+
